1bf38c47ac3e2e38c6d7bcd79783711434b009c9
liefuliu <liefuliu@liefuliu-macbookpro.roam.corp.google.com>
2015-07-16 16:30:44 -0700

    Initial Commit

---
+++ Photorama/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Photorama
+//
+//  Created by liefuliu on 7/16/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Photorama/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Photorama
+//
+//  Created by liefuliu on 7/16/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
